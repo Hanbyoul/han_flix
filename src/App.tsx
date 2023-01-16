ba55633333841f0f2c846ced1af3eab601dd17e4
Hanbyoul <dytjqdla120@naver.com>
2023-01-16 20:22:19 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import Helmets from "./Helmets";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Routes/Home";
 import Tv from "./Routes/Tv";
 import Search from "./Routes/Search";
@@ -14,33 +9,22 @@ import { ReactQueryDevtools } from "react-query/devtools";
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Router>
-          <Helmets />
-          <Header />
-          <Switch>
-            <Route
-              path={[
-                `${process.env.PUBLIC_URL}/tv`,
-                `${process.env.PUBLIC_URL}/tv/:tvId`,
-              ]}
-            >
-              <Tv />
-            </Route>
-            <Route path={[`${process.env.PUBLIC_URL}/search`]}>
-              <Search />
-            </Route>
-            <Route
-              path={[
-                `${process.env.PUBLIC_URL}/`,
-                `${process.env.PUBLIC_URL}/movies/:movieId`,
-              ]}
-            >
-              <Home />
-            </Route>
-          </Switch>
-        </Router>
-      </BrowserRouter>
+      <Router>
+        <Helmets />
+        <Header />
+        <Switch>
+          <Route path={[`/tv`, `/tv/:tvId`]}>
+            <Tv />
+          </Route>
+          <Route path={[`/search`]}>
+            <Search />
+          </Route>
+          <Route path={[`/`, `/movies/:movieId`]}>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+
       <ReactQueryDevtools initialIsOpen={true} />
     </>
   );
